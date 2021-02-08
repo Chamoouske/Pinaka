fb8ab27d05f4ce7f94c698482b2a4c1a2830e143
Yonlero <abraaobrg150@gmail.com>
2021-02-07 23:18:58 -0300

    Banco de dados adicionado

---
+++ Assets/Connection/connection_db.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+
+public class connection_db : MonoBehaviour
+{
+    string Usuario;
+
+    //Configure aqui para acessar o banco de dados, pondo o servidor, nome do banco, usuario e senha do banco respectivamente
+    MySqlConnection conn = new MySqlConnection("Server = localhost; Database=pinaka;Uid=root;Pwd=;");
+    
+    void Start()
+    {
+        conn.Open();
+        Debug.Log("Logado com sucesso");
+
+        MySqlCommand query = new MySqlCommand("SELECT * FROM usuarios", conn);
+        MySqlDataReader reader = query.ExecuteReader();
+        reader.Read();
+
+        Usuario = reader.GetString(1);
+        Debug.Log(Usuario);
+
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
